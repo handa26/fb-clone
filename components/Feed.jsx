946c567a0395bf18e0b9de349ce14f0e2628c5bb
handa26 <anandamuhammadmtq@gmail.com>
2021-11-03 11:37:09 +0800

    Create input box component & styling it

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,13 +1,17 @@
 import Stories from "./Stories";
+import InputBox from "./InputBox";
+
 // mx-auto max-w-md md:max-w-lg
 const Feed = () => {
   return (
     <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto'>
-      <div className=''>
+      <div className='mx-auto max-w-md md:max-w-lg'>
         {/* Stories */}
         <Stories />
         
         {/* InputBox Post */}
+        <InputBox />
+
         {/* Posts */}
       </div>
     </div>
