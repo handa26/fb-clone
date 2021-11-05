e992ce09d254977e8295b266f7875c7bcaea79f0
handa26 <anandamuhammadmtq@gmail.com>
2021-11-05 15:24:23 +0800

    Handle post & upload image to firbase

--- components/InputBox.jsx
+++ components/InputBox.jsx
@@ -1,4 +1,5 @@
-import { useRef } from "react";
+/* eslint-disable @next/next/no-img-element */
+import { useState, useRef } from "react";
 import { useSession } from "next-auth/client";
 import Image from "next/image";
 import firebase from "firebase";
@@ -6,11 +7,14 @@ import firebase from "firebase";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
 
-import { db } from "../firebase";
+import { db, storage } from "../firebase";
 
 const InputBox = () => {
+  const [imageToPost, setImageToPost] = useState(null);
+
   const [session] = useSession();
   const inputRef = useRef(null);
+  const filePickerRef = useRef(null);
 
   const userFirstName = session.user.name.split(" ")[0];
 
@@ -19,15 +23,62 @@ const InputBox = () => {
 
     if (!inputRef.current.value) return;
 
-    db.collection("posts").add({
-      postValue: inputRef.current.value,
-      name: session.user.name,
-      email: session.user.email,
-      image: session.user.image,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    });
+    db.collection("posts")
+      .add({
+        postValue: inputRef.current.value,
+        name: session.user.name,
+        email: session.user.email,
+        image: session.user.image,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then((doc) => {
+        if (imageToPost) {
+          const uploadTask = storage
+            .ref(`posts/${doc.id}`)
+            .putString(imageToPost, "data_url");
+
+          removeImageToPost();
+
+          uploadTask.on(
+            "state_change",
+            null,
+            (error) => console.log(error),
+            () => {
+              // When the upload completes
+              storage
+                .ref("posts")
+                .child(doc.id)
+                .getDownloadURL()
+                .then((url) => {
+                  db.collection("posts").doc(doc.id).set(
+                    {
+                      postImage: url,
+                    },
+                    { merge: true }
+                  );
+                });
+            }
+          );
+        }
+      });
 
     inputRef.current.value = "";
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+
+  const removeImageToPost = () => {
+    setImageToPost(null);
   };
 
   return (
@@ -52,6 +103,16 @@ const InputBox = () => {
             Post
           </button>
         </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImageToPost}
+            className='flex flex-col filter hover:brightness-110 transition duration-150 transfor hover:scale-105 cursor-pointer'
+          >
+            <img className='h-10 object-contain' src={imageToPost} alt='' />
+            <p className='text-xs text-red-500 text-center'>Remove</p>
+          </div>
+        )}
       </div>
 
       <div className='flex justify-evenly p-3 border-t'>
@@ -59,9 +120,18 @@ const InputBox = () => {
           <VideoCameraIcon className='h-7 text-red-500' />
           <p className='text-xs sm:text-sm xl:text-base'>Live Video</p>
         </div>
-        <div className='inputIcon'>
+        <div
+          className='inputIcon'
+          onClick={() => filePickerRef.current.click()}
+        >
           <CameraIcon className='h-7 text-green-400' />
           <p className='text-xs sm:text-sm xl:text-base'>Photo/Video</p>
+          <input
+            onChange={addImageToPost}
+            ref={filePickerRef}
+            type='file'
+            hidden
+          />
         </div>
         <div className='inputIcon'>
           <EmojiHappyIcon className='h-7 text-yellow-300' />
